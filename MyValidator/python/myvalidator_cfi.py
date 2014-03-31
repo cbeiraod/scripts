c16342af4d290214d1e44ad960a05cdc7e56c4b8
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2014-03-31 16:57:08 +0100

    Added overall/lepton gen-info output histograms. Also added a verbose flag

--- MyValidator/python/myvalidator_cfi.py
+++ MyValidator/python/myvalidator_cfi.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 validator = cms.EDAnalyzer('MyValidator',
+  verbose = cms.bool(False),
   modelTag = cms.string(""),
 
   genjets = cms.InputTag('ak5GenJets'),
--- MyValidator/src/MyValidator.cc
+++ MyValidator/src/MyValidator.cc
@@ -21,6 +21,7 @@
 // system include files
 #include <memory>
 #include <string>
+#include <iostream>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -37,6 +38,11 @@
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "DataFormats/JetReco/interface/GenJet.h"
 
+#include "DataFormats/Candidate/interface/Particle.h"
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Math/interface/LorentzVector.h"
+
+
 
 //
 // class declaration
@@ -50,6 +56,8 @@ class MyValidator : public edm::EDAnalyzer {
       static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
 
+      typedef math::PtEtaPhiMLorentzVector PolarLorentzVector;
+
    private:
       virtual void beginJob() ;
       virtual void analyze(const edm::Event&, const edm::EventSetup&);
@@ -61,10 +69,19 @@ class MyValidator : public edm::EDAnalyzer {
       virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
 
       // ----------member data ---------------------------
+      bool verbose;
+
       std::string param_modelTag_;
 
       edm::InputTag inputTag_GenParticles_;
       edm::InputTag inputTag_GenJets_;
+
+
+      TH1F* hGenMET;
+      TH1F* hGenNElectrons;
+      TH1F* hGenElectron1Pt;
+      TH1F* hGenNMuons;
+      TH1F* hGenMuon1Pt;
 };
 
 //
@@ -80,12 +97,20 @@ class MyValidator : public edm::EDAnalyzer {
 //
 MyValidator::MyValidator(const edm::ParameterSet& iConfig)
 {
+   verbose = iConfig.getParameter<bool>("verbose");
+
    param_modelTag_ = iConfig.getParameter<std::string>("modelTag");
 
    inputTag_GenParticles_ = iConfig.getParameter<edm::InputTag>("genparticles");
    inputTag_GenJets_      = iConfig.getParameter<edm::InputTag>("genjets");
 
+
    edm::Service<TFileService> rootFile;
+   hGenMET = rootFile->make<TH1F>("hGenMET", "Gen MET", 300, 0, 3000);
+   hGenNElectrons =rootFile->make<TH1F>("hGenNElectrons", "Gen NElectrons", 20, 0, 20);
+   hGenElectron1Pt = rootFile->make<TH1F>("hGenElectron1Pt", "1st Gen electron pt", 200, 0, 2000);
+   hGenNMuons =rootFile->make<TH1F>("hGenNMuons", "Gen NMuons", 20, 0, 20);
+   hGenMuon1Pt = rootFile->make<TH1F>("hGenMuon1Pt", "1st Gen muon pt", 200, 0, 2000);
 }
 
 
@@ -110,6 +135,82 @@ MyValidator::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   edm::Handle<reco::GenJetCollection> genjets;
   iEvent.getByLabel(inputTag_GenJets_, genjets);
   reco::GenJetCollection::const_iterator genjet;
+
+
+  reco::Particle::PolarLorentzVector genparticleP4 = PolarLorentzVector(0,0,0,0);
+  int    nGenElectrons=0,    nGenMuons=0;
+  double maxGenElectronPt=0, maxGenMuonPt=0;
+
+
+  for(genparticle = genparticles->begin(); genparticle != genparticles->end(); ++genparticle)
+  {
+    if(genparticle->status() == 1) //Only "stable particles"
+    {
+      if(abs(genparticle->pdgId()) != 12      && // electron neutrino
+         abs(genparticle->pdgId()) != 14      && // muon neutrino
+         abs(genparticle->pdgId()) != 16      && // tau neutrino
+         abs(genparticle->pdgId()) != 1000022 && // neutralino
+         (genparticle->p4()).E()   != 0          // Particles at rest, to remove NaN results
+         )
+      {
+        if(verbose)
+        {
+          std::cout << "Pt: " << (genparticle->p4()).pt() << std::endl;
+          if(isnan((genparticle->p4()).pt()))
+          {
+            ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > tempP4 = genparticle->p4();
+            std::cout << "\tE:" << tempP4.E() << std::endl;
+            std::cout << "\tEt:" << tempP4.Et() << std::endl;
+            std::cout << "\tEta:" << tempP4.Eta() << std::endl;
+            std::cout << "\tM:" << tempP4.M() << std::endl;
+            std::cout << "\tP2:" << tempP4.P2() << std::endl;
+            std::cout << "\tPhi:" << tempP4.Phi() << std::endl;
+            std::cout << "\tPx:" << tempP4.Px() << std::endl;
+            std::cout << "\tPy:" << tempP4.Py() << std::endl;
+            std::cout << "\tPz:" << tempP4.Pz() << std::endl;
+          }
+        }
+        genparticleP4 += genparticle->p4();
+      }
+
+      if(abs(genparticle->pdgId()) == 11) // electron
+      {
+        ++nGenElectrons;
+        if(genparticle->pt() > maxGenElectronPt)
+          maxGenElectronPt = genparticle->pt();
+      }
+
+      if(abs(genparticle->pdgId()) == 13) // muon
+      {
+        ++nGenMuons;
+        if(genparticle->pt() > maxGenMuonPt)
+          maxGenMuonPt = genparticle->pt();
+      }
+    }
+  }
+
+  if(verbose)
+  {
+    std::cout << "GenParticle MET: " << genparticleP4.pt() << std::endl;
+    std::cout << "\tE:" << genparticleP4.E() << std::endl;
+    std::cout << "\tEt:" << genparticleP4.Et() << std::endl;
+    std::cout << "\tEta:" << genparticleP4.Eta() << std::endl;
+    std::cout << "\tM:" << genparticleP4.M() << std::endl;
+    std::cout << "\tP2:" << genparticleP4.P2() << std::endl;
+    std::cout << "\tPhi:" << genparticleP4.Phi() << std::endl;
+    std::cout << "\tPx:" << genparticleP4.Px() << std::endl;
+    std::cout << "\tPy:" << genparticleP4.Py() << std::endl;
+    std::cout << "\tPz:" << genparticleP4.Pz() << std::endl;
+  }
+
+  hGenMET->Fill(genparticleP4.pt());
+  hGenNElectrons->Fill(nGenElectrons);
+  if(nGenElectrons != 0)
+    hGenElectron1Pt->Fill(maxGenElectronPt);
+  hGenNMuons->Fill(nGenMuons);
+  if(nGenMuons != 0)
+    hGenMuon1Pt->Fill(maxGenMuonPt);
+  //assert(1<0);
 }
 
 
