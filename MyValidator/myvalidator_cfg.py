99d3db790052d45fbdccd0525703f13c326c83db
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2014-04-01 18:24:46 +0100

    This updated file was missing in previous commits

--- MyValidator/myvalidator_cfg.py
+++ MyValidator/myvalidator_cfg.py
@@ -9,11 +9,12 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 inputFiles = cms.untracked.vstring()
 inputFiles.extend([
+  "file:/lustre/ncg.ingrid.pt/cmst3/store/user/cbeiraod/TStauStau/TStauStau_100_0_8E33/TStauStau_100_0_8E33v2_1_1_YIn.root",
 ])
 
 process.source = cms.Source("PoolSource",
     fileNames = inputFiles,
-    duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
+#    duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 )
 
 process.TFileService = cms.Service("TFileService",
@@ -23,6 +24,8 @@ process.TFileService = cms.Service("TFileService",
 
 #process.validator = cms.EDAnalyzer('MyValidator')
 process.load("UserCode.MyValidator.myvalidator_cfi")
+
+#process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 
 
 process.p = cms.Path(process.validator)
